019b5c6e6c24abce4578c3218886e784d4c850df
tringalematteo01-hue <tringalematteo01@gmail.com>
2026-05-18 14:57:09 +0200

    Primo esercizio con JS

---
+++ 18_05/esIfElse.js
@@ -0,0 +1,9 @@
+const password = "Matteo";
+
+let inputPass = prompt("Inserisci la password: ");
+
+if (inputPass === password) {
+    alert("Benvenuto nella dashboard!");
+} else {
+    alert("Non abilitato alla dashboard.");
+}
---
+++ 18_05/ifelse.js
@@ -0,0 +1,29 @@
+
+// costrutto IF ELSE Condizionale 
+// se si verifica la condizione, allora viene eseguito il codice all interno 1)
+// altrimenti viene eseguito il codice dentro il 2)
+
+
+if (condizione) {
+
+    // 1) fai qualcosa
+
+} else {
+
+    // fai qualcos altro
+
+};
+
+// OPERATORI DI CONFRONTO :  
+// < MINORE
+// > MAGGIORE
+// === UGUALE 
+
+var eta = parseInt(prompt("Inserisci la tua età: "));
+
+
+if (eta > 18) {
+    console.log("Sei maggiorenne");
+} else {
+    console.log("Sei minorenne");
+}
