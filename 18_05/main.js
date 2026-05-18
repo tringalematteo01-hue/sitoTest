5df2642ee9ee14437e15ddc68ef96f245e3c84e1
tringalematteo01-hue <tringalematteo01@gmail.com>
2026-05-18 14:16:45 +0200

    Inizio JS

---
+++ 18_05/main.js
@@ -0,0 +1,18 @@
+var inputIntero = parseInt(prompt("Inserisci un numero intero: "));
+var inputIntero2 = parseInt(prompt("Inserisci un numero intero: "));
+
+var somma = inputIntero + inputIntero2;
+console.log("La somma dei numeri è: " , somma);
+alert("La somma dei numeri è: " + somma);
+
+var sottrazione = inputIntero - inputIntero2;
+console.log("La sottrazione dei numeri è: " , sottrazione);
+alert("La sottrazione dei numeri è: " + sottrazione);
+
+var moltiplicazione = inputIntero * inputIntero2;
+console.log("La moltiplicazione dei numeri è: " , moltiplicazione);
+alert("La moltiplicazione dei numeri è: " + moltiplicazione)
+
+var divisione = inputIntero / inputIntero2;
+console.log("La divisione dei numeri è: " , divisione);
+alert("La divisione dei numeri è: " + divisione);
